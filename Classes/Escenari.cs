396ba8c8047cb7a50fabed707a6d885b042cbe1e
Pol Ferdman <polferdman.l@gmail.com>
2021-12-20 16:22:47 +0100

    Robot afeguit i escenari ampliat

--- Classes/Escenari.cs
+++ Classes/Escenari.cs
@@ -3,8 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Robot.Classes {
-    class Escenari {
+    class Escenari : Grid {
+        private int columnes;
+        private int files;
+
+        private int tresorX;
+        private int tresorY;
+
+        Random rng;
+
+        Image tresorImg;
+
+        public Escenari(int fil, int col) {
+            columnes = col;
+            files = fil;
+
+            rng = new Random();
+
+
+            for (int fila = 0; fila < files; fila++) {
+                RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int column = 0; column < columnes; column++) {
+                ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+
+            tresorX = rng.Next(columnes);
+            tresorY = rng.Next(files);
+
+            tresorImg.SetValue(Grid.RowProperty, tresorY);
+            tresorImg.SetValue(Grid.ColumnProperty, tresorX);
+
+            this.Children.Add(tresorImg);
+
+            this.ShowGridLines = true;
+        }
+
+
+        public bool PosValida(int x, int y) {
+            return x >= 0 && y >= 0 && x < columnes && y < files && !(x == tresorX && y == tresorY);
+        }
     }
 }
---
+++ Classes/Robot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.Classes {
+    class Robot {
+
+
+        private int posX;
+        private int posY;
+
+        public Robot(int posX, int posY) {
+            this.posX = posX;
+            this.posY = posY;
+        }
+    }
+}
